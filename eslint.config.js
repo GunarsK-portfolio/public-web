df0a6c848d484009e40ded82e90a93e040b6aeec
GunarsK <gunarskunakovs@gmail.com>
2025-10-23 11:12:44 +0300

    Add code checks on PR/dev/main push

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import pluginVue from 'eslint-plugin-vue'
 import prettierConfig from '@vue/eslint-config-prettier'
 
 export default [
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/.vite/**'],
+  },
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   prettierConfig,
